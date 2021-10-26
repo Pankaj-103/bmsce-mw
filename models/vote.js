9c28df3bd14bd9b5c5702fe2a5e3cf425bb83171
Pankaj-103 <84080470+Pankaj-103@users.noreply.github.com>
2021-10-27 02:58:58 +0530

    Update vote.js

--- models/vote.js
+++ models/vote.js
@@ -11,7 +11,7 @@ const  voteSchema= new mongoose.Schema({
     branch:{
             type:String,
             required:true,
-            enum:['CSE','ECE','CIVIL','AEROSPACE']
+            enum:['CSE','ECE','CIVIL','IS']
     },
     candidate:{
         type:String,
